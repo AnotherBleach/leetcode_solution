dd00481885f2a7686496adcd27a59ab6e9f5c514
18810987013 <2325539291@qq.com>
2021-11-08 22:30:16 +0800

    format

--- src/main/java/com/another/leetcode/simulation/java/bulls_and_cows.java
+++ src/main/java/com/another/leetcode/simulation/java/bulls_and_cows.java
@@ -1,43 +1,40 @@
 package com.another.leetcode.simulation.java;
 
 public class bulls_and_cows {
-    class Solution {
-        public String getHint(String secret, String guess) {
-            int[] data = new int[10];
-            int[] data1 = new int[10];
+    public String getHint(String secret, String guess) {
+        int[] data = new int[10];
+        int[] data1 = new int[10];
 
-            int len = secret.length();
-            int A = 0;
-            int B = 0;
+        int len = secret.length();
+        int A = 0;
+        int B = 0;
 
-            char[] sec = secret.toCharArray();
-            char[] gus = guess.toCharArray();
+        char[] sec = secret.toCharArray();
+        char[] gus = guess.toCharArray();
 
-            for (int i = 0; i < len; i++) {
-                if (sec[i] == gus[i]) {
-                    A++;
-                } else {
-                    data[sec[i] - '0']++;
-                    data1[gus[i] - '0']++;
-                }
+        for (int i = 0; i < len; i++) {
+            if (sec[i] == gus[i]) {
+                A++;
+            } else {
+                data[sec[i] - '0']++;
+                data1[gus[i] - '0']++;
             }
-            StringBuilder sb = new StringBuilder();
+        }
+        StringBuilder sb = new StringBuilder();
 
-            if (A == len)
-                return sb.append(A).append("A").append(0).append("B").toString();
-
-
-            B = 0;
-
-            for (int i = 0; i < 10; i++) {
-                if (data[i] != 0 && data1[i] != 0)
-                    B += Math.min(data[i], data1[i]);
-            }
+        if (A == len)
+            return sb.append(A).append("A").append(0).append("B").toString();
 
 
-            return sb.append(A).append("A").append(B).append("B").toString();
+        B = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (data[i] != 0 && data1[i] != 0)
+                B += Math.min(data[i], data1[i]);
         }
 
+
+        return sb.append(A).append("A").append(B).append("B").toString();
     }
 
     public static void main(String[] args) {

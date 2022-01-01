4bb85b7205415146a9adbd5e6b43737033321864
18810987013 <2325539291@qq.com>
2022-01-01 10:42:46 +0800

    happy new year~

--- src/main/java/com/another/leetcode/normal/convert_1d_array_into_2d_array.java
+++ src/main/java/com/another/leetcode/normal/convert_1d_array_into_2d_array.java
@@ -7,9 +7,10 @@ public class convert_1d_array_into_2d_array {
     public int[][] construct2DArray(int[] original, int m, int n) {
         if(m * n!=original.length)return new int[0][0];
         int[][]data = new int[m][n];
+        int id=0;
         for(int i=0;i<m;i++)
             for(int j=0;j<n;j++){
-                data[i][j] = original[i*n+j];
+                data[i][j] = original[id++];
             }
         return data;
     }

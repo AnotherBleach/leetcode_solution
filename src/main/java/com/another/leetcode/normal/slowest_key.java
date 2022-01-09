eee0b54d733dd1009f7c1f854238350695f0e0b6
18810987013 <2325539291@qq.com>
2022-01-09 17:12:34 +0800

    最长按键时间

---
+++ src/main/java/com/another/leetcode/normal/slowest_key.java
@@ -0,0 +1,21 @@
+package com.another.leetcode.normal;
+
+public class slowest_key {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        int max = releaseTimes[0];
+        char c = keysPressed.charAt(0);
+        int temp;
+        for (int j = 1; j < releaseTimes.length; j++) {
+            temp = releaseTimes[j] - releaseTimes[j - 1];
+            if(temp>max){
+                max=temp;
+                c=keysPressed.charAt(j);
+            }else if(temp==max){
+                if(keysPressed.charAt(j)>c){
+                    c=keysPressed.charAt(j);
+                }
+            }
+        }
+        return c;
+    }
+}

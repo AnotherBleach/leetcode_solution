d57f28e33a2aa5aa5faaf43973c95da3883fdeb4
18810987013 <2325539291@qq.com>
2022-04-03 10:56:58 +0800

    add another method

--- src/main/java/com/another/leetcode/binarySearch/find_smallest_letter_g_than_target.java
+++ src/main/java/com/another/leetcode/binarySearch/find_smallest_letter_g_than_target.java
@@ -25,4 +25,30 @@ public class find_smallest_letter_g_than_target {
 
         return letters[start%letters.length];
     }
+
+
+
+    public char nextGreatestLetterV2(char[] letters, char target) {
+        int start=0,stop=letters.length-1;
+        while(start<=stop){
+
+            int mid=(start+stop)/2;
+            char c= letters[mid];
+            if(c==target){
+                if(mid+1<letters.length){
+                    if(letters[mid+1]>c)return letters[mid+1];
+                    start=mid+1;
+                }else{
+                    return letters[0];
+                }
+            }else if(c>target){
+                stop=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+
+
+        return letters[start%letters.length];
+    }
 }
